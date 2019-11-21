d61b98cd266e353e9752d87c0dfd84a950c1ad4d
Marco A. Braghim <marco.a.braghim@gmail.com>
2019-11-21 20:56:09 -0300

    drag and drop estava funcionando ate aqui

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,6 @@ import 'package:flutterweb/views/widgets-list.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
-
   @override
   _MyAppState createState() => _MyAppState();
 }
@@ -16,40 +15,34 @@ class _MyAppState extends State<MyApp> {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-
     const String title = 'Flutter App Builder';
 
     window.onResize.listen((Event e) {
-      setState(() {
-        
-      });
+      setState(() {});
     });
 
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: title,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-        body: Column(
-          children: <Widget>[
-            Container(
-              height: 60,
-              color: Color(0xff111111),
-              child: Flex(
-                direction: Axis.horizontal,
-                children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.only(left: 15, right: 5),
-                    child: Icon(Icons.menu, color: Colors.white)
-                  ),
-                  Text(title, style: TextStyle(color: Colors.white))
-                ],
+        debugShowCheckedModeBanner: false,
+        title: title,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: Scaffold(
+          body: Column(
+            children: <Widget>[
+              Container(
+                height: 60,
+                color: Color(0xff111111),
+                child: Flex(
+                  direction: Axis.horizontal,
+                  children: <Widget>[
+                    Padding(padding: EdgeInsets.only(left: 15, right: 5), child: Icon(Icons.menu, color: Colors.white)),
+                    Text(title, style: TextStyle(color: Colors.white))
+                  ],
+                ),
               ),
-            ),
-            Expanded(
-              child: Row(
+              Expanded(
+                  child: Row(
                 children: <Widget>[
                   Row(children: <Widget>[
                     Container(
@@ -87,34 +80,29 @@ class _MyAppState extends State<MyApp> {
                         width: 300,
                         child: WidgetsList(),
                       ),
-                      Expanded(
-                        child: Container(
-                          color: Colors.black12,
-                          child: WidgetsSettings()
-                        )
-                      )
+                      Expanded(child: Container(color: Colors.black12, child: WidgetsSettings()))
                     ]),
                   )
                 ],
-              )
-            )
-          ],
-        ),
-      )
-    );
+              ))
+            ],
+          ),
+        ));
   }
 
   Container _appContent(String title) {
     return Container(
-      width: (window.innerWidth.toDouble()/2) - 80,
+      width: (window.innerWidth.toDouble() / 2) - 80,
       height: window.screen.available.height,
       color: Colors.black26,
-      child: Center(child: Container(
-        width: 300,
-        height: 533,
-        color: Colors.white,
-        child: MyHomePage(title: title),
-      ))
+      child: Center(
+        child: Container(
+          width: 300,
+          height: 533,
+          color: Colors.white,
+          child: MyHomePage(title: title),
+        ),
+      ),
     );
   }
 
@@ -143,15 +131,6 @@ class _MyAppState extends State<MyApp> {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -163,64 +142,69 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
+      body: DragTarget(
+        onAccept: (List<String> data) {
+          print(['onAccept', data]);
+        },
+        onLeave: (List<String> data) {
+          print(['onLeave', data]);
+        },
+        onWillAccept: (List<String> data) {
+          print(['onWillAccept', data]);
+          return true;
+        },
+        builder: (BuildContext context, list1, list2) {
+          return _getScreenContent();
+        },
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
+    );
+  }
+
+  Widget _getScreenContent() {
+    return Center(
+      // Center is a layout widget. It takes a single child and positions it
+      // in the middle of the parent.
+      child: Column(
+        // Column is also a layout widget. It takes a list of children and
+        // arranges them vertically. By default, it sizes itself to fit its
+        // children horizontally, and tries to be as tall as its parent.
+        //
+        // Invoke "debug painting" (press "p" in the console, choose the
+        // "Toggle Debug Paint" action from the Flutter Inspector in Android
+        // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+        // to see the wireframe for each widget.
+        //
+        // Column has various properties to control how it sizes itself and
+        // how it positions its children. Here we use mainAxisAlignment to
+        // center the children vertically; the main axis here is the vertical
+        // axis because Columns are vertical (the cross axis would be
+        // horizontal).
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            'You have pushed the button this many times:',
+          ),
+          Text(
+            '$_counter',
+            style: Theme.of(context).textTheme.display1,
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/views/widgets-list.dart
+++ lib/views/widgets-list.dart
@@ -6,7 +6,6 @@ class WidgetsList extends StatefulWidget {
 }
 
 class _WidgetsListState extends State<WidgetsList> {
-
   Map<String, dynamic> mapWidgetsList = {
     "Basics": {
       "expanded": true,
@@ -28,91 +27,87 @@ class _WidgetsListState extends State<WidgetsList> {
 
   @override
   Widget build(BuildContext context) {
-
     List<Widget> widgetsList = List<Widget>();
-    widgetsList.add(
-      Padding(
-        padding: EdgeInsets.only(top: 20, left: 10),
-        child: Container(
-          child: Text(
-            'Lista de Widgets',
-            style: TextStyle(fontSize: Theme.of(context).textTheme.title.fontSize),
-          ),
+    widgetsList.add(Padding(
+      padding: EdgeInsets.only(top: 20, left: 10),
+      child: Container(
+        child: Text(
+          'Lista de Widgets',
+          style: TextStyle(fontSize: Theme.of(context).textTheme.title.fontSize),
         ),
-      )
-    );
+      ),
+    ));
 
     mapWidgetsList.forEach((String category, dynamic widgets) {
-      widgetsList.add(
-        Padding(
-          padding: EdgeInsets.fromLTRB(10, 5, 10, 5),
-          child: ExpansionPanelList(
-            expansionCallback: (int i, bool status) {
-              setState(() {
-
-                // Fecha todos os outros
-                mapWidgetsList.forEach((String cat, w) {
-                  if (cat != category) {
-                    mapWidgetsList[cat]['expanded'] = false;
-                  }
-                });
-
-                // Troca o status apenas do item clicado
-                mapWidgetsList[category]['expanded'] = !status;
+      widgetsList.add(Padding(
+        padding: EdgeInsets.fromLTRB(10, 5, 10, 5),
+        child: ExpansionPanelList(
+          expansionCallback: (int i, bool status) {
+            setState(() {
+              // Fecha todos os outros
+              mapWidgetsList.forEach((String cat, w) {
+                if (cat != category) {
+                  mapWidgetsList[cat]['expanded'] = false;
+                }
               });
-            },
-            // animationDuration: Duration(milliseconds: 500),
-            children: [
-              ExpansionPanel(
-                canTapOnHeader: true,
-                headerBuilder: (BuildContext context, bool isExpanded) {
-                  return Container(
-                    padding: EdgeInsets.all(10),
-                    child: Text(category, style: Theme.of(context).textTheme.title),
-                  );
-                },
-                body: Container(
-                  child: GridView.count(
-                    crossAxisCount: 2,
-                    children: this._gridItems(widgets['items']),
-                  ),
-                  height: MediaQuery.of(context).size.height/4,
-                ),
-                isExpanded: widgets['expanded']
-              )
-            ],
-          ),
-        )
-      );
 
+              // Troca o status apenas do item clicado
+              mapWidgetsList[category]['expanded'] = !status;
+            });
+          },
+          // animationDuration: Duration(milliseconds: 500),
+          children: [
+            ExpansionPanel(
+              canTapOnHeader: true,
+              headerBuilder: (BuildContext context, bool isExpanded) {
+                return Container(
+                  padding: EdgeInsets.all(10),
+                  child: Text(category, style: Theme.of(context).textTheme.title),
+                );
+              },
+              body: Container(
+                child: GridView.count(
+                  crossAxisCount: 2,
+                  children: this._gridItems(widgets['items']),
+                ),
+                height: MediaQuery.of(context).size.height / 4,
+              ),
+              isExpanded: widgets['expanded'],
+            )
+          ],
+        ),
+      ));
     });
 
     return Column(
       mainAxisAlignment: MainAxisAlignment.start,
       crossAxisAlignment: CrossAxisAlignment.start,
-      children: widgetsList
+      children: widgetsList,
     );
   }
 
   List<Widget> _gridItems(List<String> items) {
-
     List<Widget> result = List<Widget>();
 
     items.forEach((i) {
-      result.add(
-        Padding(
-          padding: EdgeInsets.all(5),
-          child: GestureDetector(
-            onTap: () {
-              print(i);
-            },
-            child: Container(
-              color: Colors.blue,
-              child: Center(child: Text(i)),
-            ),
-          ),
-        )
+      Container container = Container(
+        color: Colors.blue,
+        child: Center(child: Text(i, style: Theme.of(context).textTheme.title)),
+        width: 100,
+        height: 100,
       );
+
+      result.add(Padding(
+        padding: EdgeInsets.all(5),
+        child: GestureDetector(
+          onTap: () => print(i),
+          child: Draggable(
+            data: [i],
+            child: container,
+            feedback: container,
+          ),
+        ),
+      ));
     });
 
     return result;
